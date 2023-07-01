ed0ac190b480edd72282ba89bf8810a8a49e4930
rajkumar-km <rajkumar.kmry@gmail.com>
2023-07-01 21:20:18 +0530

    Examples to demonstrate Variables in Go

---
+++ go-basics/variables/globals.go
@@ -0,0 +1,31 @@
+package variables
+
+import "fmt"
+
+// cmdName is a global variable accessible across the current package
+// This includes accessing from the other files that belongs to current package
+var cmdName string = "variables"
+
+// Global variable exposed even outside the current package
+// It can be accessed from other packages using <currPackageName>.GlobalVar
+var DisplayName string = initDisplayName()
+
+// init function is executed by default before the main function
+// In fact, there can be more than one init functions for the same package
+// and they are executed in the order defined. Some of the uses of init function
+// - initializing network connections prior to execution
+// - creating required files and directories
+// - checking if the dependent resources available
+func init() {
+	fmt.Println("init() function executed next")
+}
+func init() {
+	fmt.Println("multiple init() functions are executed in order")
+}
+
+// initDisplayName initialized the global variable DisplayName
+// Global variables are initialized even before the init function
+func initDisplayName() string {
+	fmt.Println("global variable initialization first")
+	return "Variables"
+}
---
+++ go-basics/variables/variables.go
@@ -0,0 +1,50 @@
+/*
+Package variables demonstrates the different ways to declare and initialize
+the Go variables.
+
+Also, explains the order in which Go runs the program
+  - Deep dependent packages are initialized first before the main package
+  - For every dependent package, Global variables are initialized first, and
+    init() functions are invoked in the order they are defined
+  - Next, the same procedure is repeated for the main package
+  - Finally, start the execution from the main() function from main package
+*/
+package variables
+
+import "fmt"
+
+// Play function demontrates the typed variables, type inference, and
+// short declarations
+func Play() {
+
+	// Typed variables (Go is statically typed language)
+	// Variables are initialized by default (0, 0.0, "", false)
+	var myInt int
+	var myFloat float32
+	var myString string
+	var myBool bool
+	fmt.Println(myInt, myFloat, myString, myBool)
+
+	// Multiple variables at once and initialization
+	var (
+		i1     int
+		f1     float32 = 3.17
+		s1, s2 string  = "hello", "world"
+		b1     bool
+	)
+	fmt.Println(i1, f1, s1, s2, b1)
+
+	// Type Inference (Go automatically infers the type of the variable based on the value provided)
+	var myVar1 = 10
+	var myVar2 = 12.5
+	var myVar3 = "hello"
+	var myVar4 = true
+	fmt.Printf("%T, %T, %T, %T\n", myVar1, myVar2, myVar3, myVar4)
+
+	// Short declaration with ":="
+	var1 := 20
+	var2 := 22.5
+	var3, var4 := "hello", "world"
+	var5 := false
+	fmt.Println(var1, var2, var3, var4, var5)
+}
