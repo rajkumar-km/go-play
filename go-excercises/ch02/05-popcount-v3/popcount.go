6e2279fd55b3441d8f7490123864d1ef3884f515
rajkumar-km <rajkumar.kmry@gmail.com>
2023-09-06 08:26:38 +0530

    Go exercise to count bits using x & (x-1) method

---
+++ go-excercises/ch02/05-popcount-v3/popcount.go
@@ -0,0 +1,87 @@
+/*
+popcount counts the number of bits set (that is 1) in given number
+This is called population count
+*/
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// Let's define a table to maintain the population count of numbers
+// from 0 to 256. This should cover a popcount for 8 bits. This table
+// can be utilized to count any types (say an uint64 can be divided 8 bytes)
+var pc [256]byte
+
+// init initializes the population count from 0 to 256
+// This will be executed before main and useful to initialize complex objects
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCountV0 returns the population count (number of set bits) of x.
+// PopCountV0 = less than 0.15 ns/op
+func PopCountV0(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+// PopCountV1 simplifies the PopCountV0 with a for range loop
+// PopCountV1 = took 3.7 ns/op in benchmark
+func PopCountV1(x uint64) int {
+	var c int = 0
+	for i := 0; i < 8; i++ {
+		c += int(pc[byte(x>>(i*8))])
+	}
+	return c
+}
+
+// PopCountV2 shifts the bits one by one instead of using a pc table
+// PopCountV2 = took 17.7 ns/op in benchmark
+func PopCountV2(x uint64) int {
+	var c int = 0
+	for i := 0; i < 64; i++ {
+		c += int((x >> i) & 1)
+	}
+	return c
+}
+
+// PopCountV3 shifts the bits one by one instead of using a pc table
+// PopCountV3 = took 5.3 ns/op in benchmark
+func PopCountV3(x uint64) int {
+	var c int = 0
+	for x != 0 {
+		x = x & (x - 1) // removes the rightmost set bit of x
+		c++
+	}
+	return c
+}
+
+// main parses a number from the command line arguments and invoke
+// different versions of popcount
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <number>\n", os.Args[0])
+		return
+	}
+	n, err := strconv.ParseUint(os.Args[1], 10, 64)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Invalid number: %s\n", os.Args[1])
+		return
+	}
+
+	fmt.Println("PopCountV0 =", PopCountV0(n))
+	fmt.Println("PopCountV1 =", PopCountV1(n))
+	fmt.Println("PopCountV2 =", PopCountV2(n))
+	fmt.Println("PopCountV3 =", PopCountV3(n))
+}
---
+++ go-excercises/ch02/05-popcount-v3/popcount_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+)
+
+// go test . -bench=. -count=5
+// PopCountV0 = less than 0.15 ns/op
+// PopCountV1 = took 3.7 ns/op
+// PopCountV2 = took 17.7 ns/op
+// PopCountV3 = took 5.3 ns/op
+
+func BenchmarkPopCountV0(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountV0(uint64(i))
+	}
+}
+
+func BenchmarkPopCountV1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountV1(uint64(i))
+	}
+}
+
+func BenchmarkPopCountV2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountV2(uint64(i))
+	}
+}
+
+func BenchmarkPopCountV3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountV3(uint64(i))
+	}
+}
