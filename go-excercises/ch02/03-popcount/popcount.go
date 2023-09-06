95a3104bb8ccaf9aee3e9ce86def4e46a78fc0d2
rajkumar-km <rajkumar.kmry@gmail.com>
2023-09-06 07:19:45 +0530

    Rename the versions for popcount functions to write future versions

--- go-excercises/ch02/03-popcount/popcount.go
+++ go-excercises/ch02/03-popcount/popcount.go
@@ -23,9 +23,9 @@ func init() {
 	}
 }
 
-// PopCountV1 returns the population count (number of set bits) of x.
-// PopCountV1 = less than 0.15 seconds
-func PopCountV1(x uint64) int {
+// PopCountV0 returns the population count (number of set bits) of x.
+// PopCountV0 = less than 0.15 seconds
+func PopCountV0(x uint64) int {
 	return int(pc[byte(x>>(0*8))] +
 		pc[byte(x>>(1*8))] +
 		pc[byte(x>>(2*8))] +
@@ -36,9 +36,9 @@ func PopCountV1(x uint64) int {
 		pc[byte(x>>(7*8))])
 }
 
-// PopCountV2 simplifies the PopCountV1 with a for range loop
-// PopcountV2 = took 3.7 seconds in benchmark
-func PopCountV2(x uint64) int {
+// PopCountV1 simplifies the PopCountV0 with a for range loop
+// PopCountV1 = took 3.7 seconds in benchmark
+func PopCountV1(x uint64) int {
 	var c int = 0
 	for i := 0; i < 8; i++ {
 		c += int(pc[byte(x>>(i*8))])
@@ -47,7 +47,7 @@ func PopCountV2(x uint64) int {
 }
 
 // main parses a number from the command line arguments and invoke
-// PopCountV1 and PopCountV2
+// PopCountV0 and PopCountV1
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Fprintf(os.Stderr, "Usage: %s <number>\n", os.Args[0])
@@ -59,6 +59,6 @@ func main() {
 		return
 	}
 
+	fmt.Println("PopCountV0 =", PopCountV0(n))
 	fmt.Println("PopCountV1 =", PopCountV1(n))
-	fmt.Println("PopCountV2 =", PopCountV2(n))
 }
--- go-excercises/ch02/03-popcount/popcount_test.go
+++ go-excercises/ch02/03-popcount/popcount_test.go
@@ -5,17 +5,17 @@ import (
 )
 
 // go test . -bench=. -count=100
-// PopCountV1 = less than 0.15 seconds
-// PopcountV2 = less than 3.7 seconds !!!
+// PopCountV0 = less than 0.15 seconds
+// PopCountV1 = less than 3.7 seconds !!!
+
+func BenchmarkPopCountV0(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountV0(uint64(i))
+	}
+}
 
 func BenchmarkPopCountV1(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		PopCountV1(uint64(i))
-	}
-}
-
-func BenchmarkPopCountV2(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		PopCountV2(uint64(i))
 	}
 }
