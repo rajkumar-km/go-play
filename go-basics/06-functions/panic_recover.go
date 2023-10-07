13c6cc2cecf30d10fc2844a28f828d5a5309328c
rajkumar-km <rajkumar.kmry@gmail.com>
2023-10-07 08:54:00 +0530

    Add Go example to recover from a panic

--- go-basics/06-functions/panic_recover.go
+++ go-basics/06-functions/panic_recover.go
@@ -10,6 +10,7 @@ import (
 // DemoPanicRecover demonstrates panic() and recover() calls in Go
 //   - Go's type system catches most of the issues at compile time, but issues like out of slice
 //     access, nil pointer access requires check at run time.
+// Panic:
 //   - Go's run time detects these mistakes and panics - Stops execution, runs all the deferred
 //     calls and program crashes with a message and stack trace.
 //   - Go’s panic mechanism runs the deferred functions before it unwinds the stack. It means we
@@ -19,25 +20,47 @@ import (
 //     is not expected reach at some point, or a missing developer's (not user's) configuration.
 //   - Note: Errors like user's misconfiguration, incorrect input, failing IO are expected to be
 //     handled gracefully without panic()
+// Recover:
+//   - Giving up is usually the best way to treat the panic since it indicates a bug.
+//   - But, sometimes we may want to recover from the situtation. Say a web server want to close
+//     the client connection when it encounters a unexpected problem.
+//   - A recover() function needs be called inside a deferred function. In such case, if the
+//     enclosing function encounters panic(), as usual the deferred functions are executed and
+//     the recover() call inside the deferred function catches the panic() error for processing
+//     and returns normally without panic.
+//   - Note that the function left the execution at the point of panic and can not execute the
+//     remaining statements.
+//   - If recover() is called without any panic then it simply returns nil.
+// Recovering from a panic without careful judgement is a bad practise:
+//   - Because the state of the package's variable is unknown
+//   - It could have incomplete update to data structures, pending locks, unclosed connections or
+//     file descriptors.
+//   - More importantly, the bugs get unnoticed by just logging the details to a file.
 func DemoPanicRecover() {
-	defer printStack()
-	defer fmt.Println("DemoPanicRecover completed")
-	for _, divisor := range []int{10, 1, 10} {
-		// This would cause panic on third iteration while dividing by zero
-		// panic: runtime error: integer divide by zero
-		// This would execute all the defer calls from the stack and panics
-		fmt.Printf("dividing 100 by %d\n", divisor)
-		divide(100, divisor)
-	}
-
-	// MustCompile causes panic if regexp is invalid
-	MustCompile("[abcd")
+	divideByZero()
+	invalidRegexp()
 }
 
-// divide performs the mathematical division
-// Note that it causes panic when the divisor is zero
-func divide(x, y int) int {
-	return x / y
+// divideByZero demonstrates panic caused when divide by zero
+func divideByZero() {
+	defer fmt.Println("divideByZero completed")
+	defer recoverFromErrors() // recover()
+
+	for _, divisor := range []int{10, 0} {
+		// This would cause panic on second iteration while dividing by zero
+		// This would execute all the defer calls from the stack and panics
+		fmt.Printf("dividing 100 by %d\n", divisor)
+		fmt.Printf("%d\n", 100/divisor) // panic: runtime error: integer divide by zero
+	}
+}
+
+// recoverFromErrors is intended to be a deferred function to recover from panics
+func recoverFromErrors() {
+	p := recover()
+	if p != nil {
+		fmt.Println("Function encountered a panic:")
+		printStack()
+	}
 }
 
 // printStack prints the current stack trace
@@ -47,6 +70,20 @@ func printStack() {
 	var buf [4096]byte
 	n := runtime.Stack(buf[:], false)
 	os.Stdout.Write(buf[:n])
+}
+
+// invalidRegexp demonstrates invoking panic()
+func invalidRegexp() (retErr error) {
+	defer func() {
+		if p := recover(); p != nil {
+			// Prevents the function from panic and modify its return value as error
+			retErr = fmt.Errorf("internal error: %v", p)			
+		}
+	}()
+
+	// MustCompile causes panic if regexp is invalid
+	MustCompile("[abcd")
+	return nil
 }
 
 // MustCompile compiles a regular expression and causes panic if it is invalid
