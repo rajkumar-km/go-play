99fb091d18b8965c52273d016b6e0b026bb350f5
rajkumar-km <rajkumar.kmry@gmail.com>
2023-10-07 14:33:07 +0530

    Add example to recover from a selective panics

--- go-basics/06-functions/panic_recover.go
+++ go-basics/06-functions/panic_recover.go
@@ -10,6 +10,7 @@ import (
 // DemoPanicRecover demonstrates panic() and recover() calls in Go
 //   - Go's type system catches most of the issues at compile time, but issues like out of slice
 //     access, nil pointer access requires check at run time.
+//
 // Panic:
 //   - Go's run time detects these mistakes and panics - Stops execution, runs all the deferred
 //     calls and program crashes with a message and stack trace.
@@ -20,6 +21,7 @@ import (
 //     is not expected reach at some point, or a missing developer's (not user's) configuration.
 //   - Note: Errors like user's misconfiguration, incorrect input, failing IO are expected to be
 //     handled gracefully without panic()
+//
 // Recover:
 //   - Giving up is usually the best way to treat the panic since it indicates a bug.
 //   - But, sometimes we may want to recover from the situtation. Say a web server want to close
@@ -31,14 +33,25 @@ import (
 //   - Note that the function left the execution at the point of panic and can not execute the
 //     remaining statements.
 //   - If recover() is called without any panic then it simply returns nil.
+//
 // Recovering from a panic without careful judgement is a bad practise:
 //   - Because the state of the package's variable is unknown
 //   - It could have incomplete update to data structures, pending locks, unclosed connections or
 //     file descriptors.
 //   - More importantly, the bugs get unnoticed by just logging the details to a file.
+//   - Recovering from a same package is ideal can handle the unexpected errors by closing things.
+//   - But, do not recover from a panic caused by other packages as a general rule. All the
+//     public APIs should report failures as errors instead of panic.
+//   - So, recover() can be used for the functions that we own.
+//   - In some cases, say net/http provides a web server that dispatches incoming request to user
+//     provided handlers. This would recover() from any panics caused by the handlers to avoid the
+//     web service to be down. But it is risky since it recovers without knowing their state.
+//   - In such cases, you may recover only from selective panics. Since the panic(any) posts any
+//     type value, and recover() can obtain the value.
 func DemoPanicRecover() {
 	divideByZero()
 	invalidRegexp()
+	selectiveRecover()
 }
 
 // divideByZero demonstrates panic caused when divide by zero
@@ -77,7 +90,7 @@ func invalidRegexp() (retErr error) {
 	defer func() {
 		if p := recover(); p != nil {
 			// Prevents the function from panic and modify its return value as error
-			retErr = fmt.Errorf("internal error: %v", p)			
+			retErr = fmt.Errorf("internal error: %v", p)
 		}
 	}()
 
@@ -94,4 +107,25 @@ func MustCompile(expr string) *regexp.Regexp {
 		panic(err) // panic accepts the argument of type "any"
 	}
 	return re
+}
+
+// selectiveRecover demonstrates recovering from selected panics
+func selectiveRecover() {
+	type knownPanic struct{}
+
+	defer func() {
+		switch p := recover(); p {
+		case nil:
+			// No panic
+		case knownPanic{}:
+			// Recover from the selective panic
+			fmt.Println("Expected panic during selectiveRecover")
+		default:
+			// Other cases, let the panic crash the program
+			panic(p)
+		}
+	}()
+
+	// Assume some case we would want to raise panic
+	panic(knownPanic{})
 }
