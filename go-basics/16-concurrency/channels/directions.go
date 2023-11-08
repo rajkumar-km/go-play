402c55f90bed68c2221c7fbb708af07173e2714e
rajkumar-km <rajkumar.kmry@gmail.com>
2023-11-08 08:05:42 +0530

    Add samples to use send-only channels in Go

--- go-basics/16-concurrency/channels/directions.go
+++ go-basics/16-concurrency/channels/directions.go
@@ -10,10 +10,25 @@ import "fmt"
 func DemoChannelDirections() {
 	msgQueue := make(chan string, 2)
 
+	// Directions are useful when:
+	// - passing channel as arguments
+	// - storing channels in a data structure that is used either for sending or receiving
+
 	// See sendMsg() and receiveMsg() and note the type of arguments used to pass the channel
-	// ---------------------------
 	sendMsg(msgQueue)
 	receiveMsg(msgQueue)
+
+	// Here, a map structure can store a channel as send only for broadcasting
+	type client chan<- string
+	broadcasterClients := make(map[client]bool)
+	broadcasterClients[msgQueue] = true
+
+	// So, a channel can be intermediately used only for sending or receiving for safety,
+	// and indicating the intent of how it should be used.
+	// However, creating unidirectional channel does not help although it is allowed
+	ch := make(chan<- int, 1)
+	ch <- 1
+	// x := <-ch // compile error: can not receiving from send only channel
 }
 
 // A channel argument can be specified only for sending messages
